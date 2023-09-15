059995ae68e740b7d688513c9656f1ad93c75249
DevExpressExampleBot <fakeEmail>
2023-09-15 16:08:17 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace SchedulerFindAptByRow
-    Partial Public Class Form1
+Namespace SchedulerFindAptByRow
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,25 +12,25 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
-            Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler1 As DevExpress.XtraScheduler.TimeRuler = New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler2 As DevExpress.XtraScheduler.TimeRuler = New DevExpress.XtraScheduler.TimeRuler()
             Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
             Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
-            Me.carsDBDataSet_Renamed = New SchedulerFindAptByRow.CarsDBDataSet()
+            Me.carsDBDataSet = New SchedulerFindAptByRow.CarsDBDataSet()
             Me.carSchedulingTableAdapter = New SchedulerFindAptByRow.CarsDBDataSetTableAdapters.CarSchedulingTableAdapter()
             Me.carsTableAdapter = New SchedulerFindAptByRow.CarsDBDataSetTableAdapters.CarsTableAdapter()
             Me.dataGridView1 = New System.Windows.Forms.DataGridView()
@@ -41,10 +43,10 @@
             Me.endTimeDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
             Me.eventTypeDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
             Me.recurrenceInfoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.carsDBDataSet_Renamed, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.dataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.schedulerControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.schedulerStorage1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.carsDBDataSet), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataGridView1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.splitContainer1.Panel1.SuspendLayout()
             Me.splitContainer1.Panel2.SuspendLayout()
             Me.splitContainer1.SuspendLayout()
@@ -62,18 +64,19 @@
             Me.schedulerControl1.OptionsView.ResourceHeaders.ImageSize = New System.Drawing.Size(100, 100)
             Me.schedulerControl1.OptionsView.ResourceHeaders.ImageSizeMode = DevExpress.XtraScheduler.HeaderImageSizeMode.ZoomImage
             Me.schedulerControl1.Size = New System.Drawing.Size(607, 695)
-            Me.schedulerControl1.Start = New Date(2008, 10, 20, 0, 0, 0, 0)
+            Me.schedulerControl1.Start = New System.DateTime(2008, 10, 20, 0, 0, 0, 0)
             Me.schedulerControl1.Storage = Me.schedulerStorage1
             Me.schedulerControl1.TabIndex = 0
             Me.schedulerControl1.Text = "schedulerControl1"
             Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
             Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+            AddHandler Me.schedulerControl1.Click, New System.EventHandler(AddressOf Me.schedulerControl1_Click)
             ' 
             ' schedulerStorage1
             ' 
             Me.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay"
             Me.schedulerStorage1.Appointments.Mappings.Description = "Description"
-            Me.schedulerStorage1.Appointments.Mappings.End = "EndTime"
+            Me.schedulerStorage1.Appointments.Mappings.[End] = "EndTime"
             Me.schedulerStorage1.Appointments.Mappings.Label = "Label"
             Me.schedulerStorage1.Appointments.Mappings.Location = "Location"
             Me.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo"
@@ -86,11 +89,15 @@
             Me.schedulerStorage1.Resources.Mappings.Caption = "Model"
             Me.schedulerStorage1.Resources.Mappings.Id = "ID"
             Me.schedulerStorage1.Resources.Mappings.Image = "Picture"
+            AddHandler Me.schedulerStorage1.AppointmentsInserted, New DevExpress.XtraScheduler.PersistentObjectsEventHandler(AddressOf Me.schedulerStorage1_AppointmentsInserted)
+            AddHandler Me.schedulerStorage1.AppointmentCollectionLoaded, New System.EventHandler(AddressOf Me.schedulerStorage1_AppointmentCollectionLoaded)
+            AddHandler Me.schedulerStorage1.AppointmentCollectionCleared, New System.EventHandler(AddressOf Me.schedulerStorage1_AppointmentCollectionCleared)
+            AddHandler Me.schedulerStorage1.AppointmentsDeleted, New DevExpress.XtraScheduler.PersistentObjectsEventHandler(AddressOf Me.schedulerStorage1_AppointmentsDeleted)
             ' 
             ' carsDBDataSet
             ' 
-            Me.carsDBDataSet_Renamed.DataSetName = "CarsDBDataSet"
-            Me.carsDBDataSet_Renamed.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            Me.carsDBDataSet.DataSetName = "CarsDBDataSet"
+            Me.carsDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
             ' 
             ' carSchedulingTableAdapter
             ' 
@@ -107,20 +114,23 @@
             Me.dataGridView1.AutoGenerateColumns = False
             Me.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
             Me.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-            Me.dataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() { Me.Number, Me.iDDataGridViewTextBoxColumn, Me.subjectDataGridViewTextBoxColumn, Me.startTimeDataGridViewTextBoxColumn, Me.endTimeDataGridViewTextBoxColumn, Me.eventTypeDataGridViewTextBoxColumn, Me.recurrenceInfoDataGridViewTextBoxColumn})
+            Me.dataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Number, Me.iDDataGridViewTextBoxColumn, Me.subjectDataGridViewTextBoxColumn, Me.startTimeDataGridViewTextBoxColumn, Me.endTimeDataGridViewTextBoxColumn, Me.eventTypeDataGridViewTextBoxColumn, Me.recurrenceInfoDataGridViewTextBoxColumn})
             Me.dataGridView1.DataMember = "CarScheduling"
-            Me.dataGridView1.DataSource = Me.carsDBDataSet_Renamed
+            Me.dataGridView1.DataSource = Me.carsDBDataSet
             Me.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.dataGridView1.Location = New System.Drawing.Point(0, 0)
             Me.dataGridView1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
             Me.dataGridView1.MultiSelect = False
             Me.dataGridView1.Name = "dataGridView1"
-            Me.dataGridView1.ReadOnly = True
+            Me.dataGridView1.[ReadOnly] = True
             Me.dataGridView1.RowTemplate.Height = 24
             Me.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
             Me.dataGridView1.Size = New System.Drawing.Size(839, 695)
             Me.dataGridView1.TabIndex = 4
             Me.dataGridView1.VirtualMode = True
+            AddHandler Me.dataGridView1.CellValueNeeded, New System.Windows.Forms.DataGridViewCellValueEventHandler(AddressOf Me.dataGridView1_CellValueNeeded)
+            AddHandler Me.dataGridView1.CellFormatting, New System.Windows.Forms.DataGridViewCellFormattingEventHandler(AddressOf Me.dataGridView1_CellFormatting)
+            AddHandler Me.dataGridView1.SelectionChanged, New System.EventHandler(AddressOf Me.dataGridView1_SelectionChanged)
             ' 
             ' splitContainer1
             ' 
@@ -145,8 +155,8 @@
             ' 
             ' lbNoAppointment
             ' 
-            Me.lbNoAppointment.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-            Me.lbNoAppointment.Font = New System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(204)))
+            Me.lbNoAppointment.Anchor = CType(((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.lbNoAppointment.Font = New System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte((204))))
             Me.lbNoAppointment.Location = New System.Drawing.Point(172, 205)
             Me.lbNoAppointment.Name = "lbNoAppointment"
             Me.lbNoAppointment.Size = New System.Drawing.Size(257, 271)
@@ -154,13 +164,14 @@
             Me.lbNoAppointment.Text = "Cannot display appointment for selected row"
             Me.lbNoAppointment.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
             Me.lbNoAppointment.Visible = False
+            AddHandler Me.lbNoAppointment.Click, New System.EventHandler(AddressOf Me.schedulerControl1_Click)
             ' 
             ' Number
             ' 
             Me.Number.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
             Me.Number.HeaderText = "#"
             Me.Number.Name = "Number"
-            Me.Number.ReadOnly = True
+            Me.Number.[ReadOnly] = True
             Me.Number.Width = 41
             ' 
             ' iDDataGridViewTextBoxColumn
@@ -170,7 +181,7 @@
             Me.iDDataGridViewTextBoxColumn.FillWeight = 33.50254F
             Me.iDDataGridViewTextBoxColumn.HeaderText = "ID"
             Me.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn"
-            Me.iDDataGridViewTextBoxColumn.ReadOnly = True
+            Me.iDDataGridViewTextBoxColumn.[ReadOnly] = True
             Me.iDDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
             Me.iDDataGridViewTextBoxColumn.Width = 27
             ' 
@@ -181,7 +192,7 @@
             Me.subjectDataGridViewTextBoxColumn.FillWeight = 33.50254F
             Me.subjectDataGridViewTextBoxColumn.HeaderText = "Subject"
             Me.subjectDataGridViewTextBoxColumn.Name = "subjectDataGridViewTextBoxColumn"
-            Me.subjectDataGridViewTextBoxColumn.ReadOnly = True
+            Me.subjectDataGridViewTextBoxColumn.[ReadOnly] = True
             Me.subjectDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
             Me.subjectDataGridViewTextBoxColumn.Width = 61
             ' 
@@ -192,7 +203,7 @@
             Me.startTimeDataGridViewTextBoxColumn.FillWeight = 33.50254F
             Me.startTimeDataGridViewTextBoxColumn.HeaderText = "StartTime"
             Me.startTimeDataGridViewTextBoxColumn.Name = "startTimeDataGridViewTextBoxColumn"
-            Me.startTimeDataGridViewTextBoxColumn.ReadOnly = True
+            Me.startTimeDataGridViewTextBoxColumn.[ReadOnly] = True
             Me.startTimeDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
             Me.startTimeDataGridViewTextBoxColumn.Width = 75
             ' 
@@ -203,7 +214,7 @@
             Me.endTimeDataGridViewTextBoxColumn.FillWeight = 33.50254F
             Me.endTimeDataGridViewTextBoxColumn.HeaderText = "EndTime"
             Me.endTimeDataGridViewTextBoxColumn.Name = "endTimeDataGridViewTextBoxColumn"
-            Me.endTimeDataGridViewTextBoxColumn.ReadOnly = True
+            Me.endTimeDataGridViewTextBoxColumn.[ReadOnly] = True
             Me.endTimeDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
             Me.endTimeDataGridViewTextBoxColumn.Width = 70
             ' 
@@ -214,7 +225,7 @@
             Me.eventTypeDataGridViewTextBoxColumn.FillWeight = 432.4873F
             Me.eventTypeDataGridViewTextBoxColumn.HeaderText = "EventType"
             Me.eventTypeDataGridViewTextBoxColumn.Name = "eventTypeDataGridViewTextBoxColumn"
-            Me.eventTypeDataGridViewTextBoxColumn.ReadOnly = True
+            Me.eventTypeDataGridViewTextBoxColumn.[ReadOnly] = True
             Me.eventTypeDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
             Me.eventTypeDataGridViewTextBoxColumn.Width = 82
             ' 
@@ -224,7 +235,7 @@
             Me.recurrenceInfoDataGridViewTextBoxColumn.FillWeight = 33.50254F
             Me.recurrenceInfoDataGridViewTextBoxColumn.HeaderText = "RecurrenceInfo"
             Me.recurrenceInfoDataGridViewTextBoxColumn.Name = "recurrenceInfoDataGridViewTextBoxColumn"
-            Me.recurrenceInfoDataGridViewTextBoxColumn.ReadOnly = True
+            Me.recurrenceInfoDataGridViewTextBoxColumn.[ReadOnly] = True
             Me.recurrenceInfoDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
             ' 
             ' Form1
@@ -236,35 +247,46 @@
             Me.Margin = New System.Windows.Forms.Padding(4)
             Me.Name = "Form1"
             Me.Text = "Find appointment by row"
-            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.carsDBDataSet_Renamed, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.dataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.schedulerControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.schedulerStorage1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.carsDBDataSet), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataGridView1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.splitContainer1.Panel1.ResumeLayout(False)
             Me.splitContainer1.Panel2.ResumeLayout(False)
             Me.splitContainer1.ResumeLayout(False)
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
+'#End Region
+        Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
 
-        Private WithEvents schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+        Private carsDBDataSet As SchedulerFindAptByRow.CarsDBDataSet
 
-        Private carsDBDataSet_Renamed As CarsDBDataSet
-        Private WithEvents schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+        Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+
         Private carSchedulingTableAdapter As SchedulerFindAptByRow.CarsDBDataSetTableAdapters.CarSchedulingTableAdapter
+
         Private carsTableAdapter As SchedulerFindAptByRow.CarsDBDataSetTableAdapters.CarsTableAdapter
-        Private WithEvents dataGridView1 As System.Windows.Forms.DataGridView
+
+        Private dataGridView1 As System.Windows.Forms.DataGridView
+
         Private splitContainer1 As System.Windows.Forms.SplitContainer
-        Private WithEvents lbNoAppointment As System.Windows.Forms.Label
+
+        Private lbNoAppointment As System.Windows.Forms.Label
+
         Private Number As System.Windows.Forms.DataGridViewTextBoxColumn
+
         Private iDDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+
         Private subjectDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+
         Private startTimeDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+
         Private endTimeDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+
         Private eventTypeDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+
         Private recurrenceInfoDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
     End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,93 +1,90 @@
-﻿Imports System
+Imports System
 Imports System.Data
 Imports System.Windows.Forms
 Imports DevExpress.XtraScheduler
 Imports System.Collections.Generic
 
 Namespace SchedulerFindAptByRow
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
+        Private appointmentRowDictionaryField As Dictionary(Of Object, Appointment) = New Dictionary(Of Object, Appointment)()
 
-        Private appointmentRowDictionary_Renamed As New Dictionary(Of Object, Appointment)()
-
-        Private rowAppointmentDictionary_Renamed As New Dictionary(Of Appointment, Object)()
+        Private rowAppointmentDictionaryField As Dictionary(Of Appointment, Object) = New Dictionary(Of Appointment, Object)()
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Protected ReadOnly Property AppointmentRowDictionary() As Dictionary(Of Object, Appointment)
+        Protected ReadOnly Property AppointmentRowDictionary As Dictionary(Of Object, Appointment)
             Get
-                Return appointmentRowDictionary_Renamed
-            End Get
-        End Property
-        Protected ReadOnly Property RowAppointmentDictionary() As Dictionary(Of Appointment, Object)
-            Get
-                Return rowAppointmentDictionary_Renamed
+                Return appointmentRowDictionaryField
             End Get
         End Property
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Protected ReadOnly Property RowAppointmentDictionary As Dictionary(Of Appointment, Object)
+            Get
+                Return rowAppointmentDictionaryField
+            End Get
+        End Property
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
-            Me.carsTableAdapter.Fill(Me.carsDBDataSet_Renamed.Cars)
+            carsTableAdapter.Fill(carsDBDataSet.Cars)
             ' TODO: This line of code loads data into the 'carsDBDataSet.CarScheduling' table. You can move, or remove it, as needed.
-            Me.carSchedulingTableAdapter.Fill(Me.carsDBDataSet_Renamed.CarScheduling)
-
-            schedulerStorage1.Resources.DataSource = carsDBDataSet_Renamed.Cars
-            schedulerStorage1.Appointments.DataSource = carsDBDataSet_Renamed.CarScheduling
-
-            dataGridView1.DataSource = carsDBDataSet_Renamed.CarScheduling
-
-            If schedulerStorage1.Appointments.Count > 0 Then
-                schedulerControl1.Start = schedulerStorage1.Appointments(0).Start
-            End If
+            carSchedulingTableAdapter.Fill(carsDBDataSet.CarScheduling)
+            schedulerStorage1.Resources.DataSource = carsDBDataSet.Cars
+            schedulerStorage1.Appointments.DataSource = carsDBDataSet.CarScheduling
+            dataGridView1.DataSource = carsDBDataSet.CarScheduling
+            If schedulerStorage1.Appointments.Count > 0 Then schedulerControl1.Start = schedulerStorage1.Appointments(0).Start
         End Sub
 
-        Private Sub schedulerStorage1_AppointmentCollectionLoaded(ByVal sender As Object, ByVal e As EventArgs) Handles schedulerStorage1.AppointmentCollectionLoaded
+        Private Sub schedulerStorage1_AppointmentCollectionLoaded(ByVal sender As Object, ByVal e As EventArgs)
             For Each apt As Appointment In schedulerStorage1.Appointments.Items
-                Dim apts As New AppointmentBaseCollection()
+                Dim apts As AppointmentBaseCollection = New AppointmentBaseCollection()
                 apts.Add(apt)
                 RegisterAppointmentRows(apts)
                 If apt.Type = AppointmentType.Pattern Then
                     RegisterAppointmentRows(apt.GetExceptions())
                 End If
-            Next apt
+            Next
         End Sub
 
-        Private Sub schedulerStorage1_AppointmentCollectionCleared(ByVal sender As Object, ByVal e As EventArgs) Handles schedulerStorage1.AppointmentCollectionCleared
+        Private Sub schedulerStorage1_AppointmentCollectionCleared(ByVal sender As Object, ByVal e As EventArgs)
             AppointmentRowDictionary.Clear()
             RowAppointmentDictionary.Clear()
         End Sub
 
-        Private Sub schedulerStorage1_AppointmentsInserted(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs) Handles schedulerStorage1.AppointmentsInserted
+        Private Sub schedulerStorage1_AppointmentsInserted(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
             Dim apts As AppointmentBaseCollection = CType(e.Objects, AppointmentBaseCollection)
             For Each apt As Appointment In apts
                 Dim row As DataRow = GetAppointmentRow(apt)
                 RegisterAppointmentRow(row, apt)
-            Next apt
+            Next
         End Sub
 
-        Private Sub schedulerStorage1_AppointmentsDeleted(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs) Handles schedulerStorage1.AppointmentsDeleted
+        Private Sub schedulerStorage1_AppointmentsDeleted(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
             Dim apts As AppointmentBaseCollection = CType(e.Objects, AppointmentBaseCollection)
             For Each apt As Appointment In apts
-                Dim row As DataRow = DirectCast(RowAppointmentDictionary(apt), DataRow)
+                Dim row As DataRow = CType(RowAppointmentDictionary(apt), DataRow)
                 AppointmentRowDictionary.Remove(row)
                 RowAppointmentDictionary.Remove(apt)
-            Next apt
+            Next
         End Sub
 
         Private Sub RegisterAppointmentRows(ByVal apts As AppointmentBaseCollection)
             For Each apt As Appointment In apts
                 Dim row As DataRow = GetAppointmentRow(apt)
                 RegisterAppointmentRow(row, apt)
-            Next apt
+            Next
         End Sub
 
         Private Sub RegisterAppointmentRow(ByVal row As DataRow, ByVal apt As Appointment)
             If Not AppointmentRowDictionary.ContainsKey(row) Then
                 AppointmentRowDictionary.Add(row, apt)
             End If
+
             If Not RowAppointmentDictionary.ContainsKey(apt) Then
                 RowAppointmentDictionary.Add(apt, row)
             End If
@@ -105,25 +102,25 @@ Namespace SchedulerFindAptByRow
             End If
         End Function
 
-        Private Sub dataGridView1_SelectionChanged(ByVal sender As Object, ByVal e As EventArgs) Handles dataGridView1.SelectionChanged
+        Private Sub dataGridView1_SelectionChanged(ByVal sender As Object, ByVal e As EventArgs)
             UpdateLabelVisible(False)
             If dataGridView1.SelectedRows.Count <> 0 Then
                 Dim rowIndex As Integer = dataGridView1.Rows.IndexOf(dataGridView1.SelectedRows(0))
-                Dim row As DataRow = Me.carsDBDataSet_Renamed.CarScheduling.Rows(rowIndex)
+                Dim row As DataRow = carsDBDataSet.CarScheduling.Rows(rowIndex)
                 Dim apt As Appointment = FindAppointmentByRow(row)
-
                 schedulerControl1.Services.AppointmentSelection.ClearSelection()
                 If apt IsNot Nothing Then
                     If apt.Type = AppointmentType.Pattern OrElse apt.Type = AppointmentType.DeletedOccurrence Then
                         UpdateLabelVisible(True)
                         Return
                     End If
+
                     schedulerControl1.ActiveView.SelectAppointment(apt)
                 End If
             End If
         End Sub
 
-        Private Sub schedulerControl1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles schedulerControl1.Click, lbNoAppointment.Click
+        Private Sub schedulerControl1_Click(ByVal sender As Object, ByVal e As EventArgs)
             UpdateLabelVisible(False)
         End Sub
 
@@ -131,16 +128,12 @@ Namespace SchedulerFindAptByRow
             lbNoAppointment.Visible = visible
         End Sub
 
-        Private Sub dataGridView1_CellFormatting(ByVal sender As Object, ByVal e As DataGridViewCellFormattingEventArgs) Handles dataGridView1.CellFormatting
-            If dataGridView1.Columns(e.ColumnIndex).DataPropertyName = "EventType" Then
-                e.Value = CType(Convert.ToInt32(e.Value), AppointmentType).ToString()
-            End If
+        Private Sub dataGridView1_CellFormatting(ByVal sender As Object, ByVal e As DataGridViewCellFormattingEventArgs)
+            If Equals(dataGridView1.Columns(e.ColumnIndex).DataPropertyName, "EventType") Then e.Value = CType(Convert.ToInt32(e.Value), AppointmentType).ToString()
         End Sub
 
-        Private Sub dataGridView1_CellValueNeeded(ByVal sender As Object, ByVal e As DataGridViewCellValueEventArgs) Handles dataGridView1.CellValueNeeded
-            If dataGridView1.Columns(e.ColumnIndex).Name = "Number" Then
-                e.Value = e.RowIndex
-            End If
+        Private Sub dataGridView1_CellValueNeeded(ByVal sender As Object, ByVal e As DataGridViewCellValueEventArgs)
+            If Equals(dataGridView1.Columns(e.ColumnIndex).Name, "Number") Then e.Value = e.RowIndex
         End Sub
     End Class
 End Namespace
